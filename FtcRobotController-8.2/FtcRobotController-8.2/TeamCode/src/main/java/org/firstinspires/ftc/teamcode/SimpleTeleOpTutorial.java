716a09f25771b29b282e5d271e156e3cd42aab71
chickenfingers68 <jaquelynnguyen373@gmail.com>
2023-08-23 22:30:37 -0700

    Redid

--- FtcRobotController-8.2/FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOpTutorial.java
+++ FtcRobotController-8.2/FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOpTutorial.java
@@ -28,22 +28,20 @@ public class SimpleTeleOpTutorial extends LinearOpMode {
         front_right = hardwareMap.get(DcMotor.class, "front_right");
         back_left = hardwareMap.get(DcMotor.class, "back_left");
         back_right = hardwareMap.get(DcMotor.class, "back_right");
-        slide_left = hardwareMap.get(DcMotor.class, "back_left");
-        slide_right = hardwareMap.get(DcMotor.class, "back_right");
-        arm_left = hardwareMap.get(CRServo.class, "left_arm");
-        arm_right = hardwareMap.get(CRServo.class, "right_arm");
+        slide_left = hardwareMap.get(DcMotor.class, "slide_left");
+        slide_right = hardwareMap.get(DcMotor.class, "slide_right");
         claw = hardwareMap.get(Servo.class, "claw");
+        arm_left = hardwareMap.get(CRServo.class, "arm_left");
+        arm_right = hardwareMap.get(CRServo.class, "arm_right");
 
         front_left.setDirection(DcMotorSimple.Direction.REVERSE);
         back_left.setDirection(DcMotorSimple.Direction.REVERSE);
         arm_right.setDirection(DcMotorSimple.Direction.REVERSE);
-        slide_left.setDirection(DcMotorSimple.Direction.REVERSE);
         slide_right.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        claw.setPosition(CLOSED_CLAW);
+        slide_left.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
-        while (opModeIsActive()) {
+        while(opModeIsActive()) {
             double forward = -gamepad1.left_stick_y;
             double strafe = gamepad1.left_stick_x;
             double rotate = gamepad1.right_stick_x;
@@ -54,17 +52,18 @@ public class SimpleTeleOpTutorial extends LinearOpMode {
             double fRPower = forward - rotate - strafe;
             double bLPower = forward + rotate - strafe;
             double bRPower = forward - rotate + strafe;
+
             setDrivePowers(fLPower, fRPower, bLPower, bRPower);
 
             //claw code
-            if (gamepad2.left_trigger > 0.1 || gamepad2.b) {
+            if(gamepad2.left_trigger > 0.1 || gamepad2.b) {
                 claw.setPosition(OPEN_CLAW);
             } else if (gamepad2.right_trigger > 0.1 || gamepad2.x) {
                 claw.setPosition(CLOSED_CLAW);
             }
 
             //arm code
-            if (arms > 0.2) {
+            if(arms > 0.2) {
                 arm_left.setPower(1);
                 arm_right.setPower(1);
             } else if (arms < -0.2) {
@@ -83,37 +82,39 @@ public class SimpleTeleOpTutorial extends LinearOpMode {
                 slide_left.setPower(-0.775);
                 slide_right.setPower(-0.775);
             } else {
-                slide_left.setPower(0.04);
-                slide_right.setPower(0.04);
+                slide_left.setPower(0);
+                slide_right.setPower(0);
             }
 
-            telemetry.addData("arm: ", arm_left);
-            telemetry.addData("slides: ", slide_left);
+            telemetry.addData("clawPos: ", claw.getPosition());
+            telemetry.addData("arms: ", arm_left.getPower());
+            telemetry.addData("slides: ", slide_left.getPower());
             telemetry.update();
         }
     }
 
-    private void setDrivePowers(double fLPower, double fRPower, double bLPower, double bRPower) {
+    private void setDrivePowers(double fLPower, double fRPower, double bLPower, double bRPower){
+
         double maxSpeed = 1.0;
-        maxSpeed = Math.max(maxSpeed, Math.abs(bLPower));
-        maxSpeed = Math.max(maxSpeed, Math.abs(bRPower));
         maxSpeed = Math.max(maxSpeed, Math.abs(fLPower));
         maxSpeed = Math.max(maxSpeed, Math.abs(fRPower));
+        maxSpeed = Math.max(maxSpeed, Math.abs(bLPower));
+        maxSpeed = Math.max(maxSpeed, Math.abs(bRPower));
 
-        bLPower /= maxSpeed;
-        bRPower /= maxSpeed;
         fLPower /= maxSpeed;
         fRPower /= maxSpeed;
+        bLPower /= maxSpeed;
+        bRPower /= maxSpeed;
 
-        back_left.setPower(bLPower);
-        back_right.setPower(bRPower);
         front_left.setPower(fLPower);
         front_right.setPower(fRPower);
+        back_left.setPower(bLPower);
+        back_right.setPower(bRPower);
 
+        telemetry.addData("fL: ", fLPower);
+        telemetry.addData("fR: ", fLPower);
+        telemetry.addData("bL: ", fLPower);
+        telemetry.addData("bR: ", fLPower);
 
-        telemetry.addData("front_left: ", fLPower);
-        telemetry.addData("front_right: ", fRPower);
-        telemetry.addData("back_left: ", bLPower);
-        telemetry.addData("back_right: ", bRPower);
     }
 }
